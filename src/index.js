0ca7cfd77c9e49283d26940021cde924392857f5
thayron <thayronf3@gmail.com>
2021-03-20 15:15:56 -0300

    Revert "Form feitos todos os teste e criaro o README"
    This reverts commit f6940be4f6dad844f29d2a27f7249a897fa642cb.

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ app.use(express.json());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-
   return response.json(repositories);
 });
 
@@ -23,27 +22,21 @@ app.post("/repositories", (request, response) => {
     techs,
     likes: 0
   };
-  repositories.push(repository)
 
   return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  const { title = false, url = false, techs = false } = request.body
+  const updatedRepository = request.body;
 
-  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  repositoryIndex = repositories.findindex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  const repository = {
-    ...repositories[repositoryIndex],
-    title,
-    url,
-    techs
-  };
+  const repository = { ...repositories[repositoryIndex], ...updatedRepository };
 
   repositories[repositoryIndex] = repository;
 
@@ -53,9 +46,9 @@ app.put("/repositories/:id", (request, response) => {
 app.delete("/repositories/:id", (request, response) => {
   const { id } = request.params;
 
-  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex < 0) {
+  if (repositoryIndex > 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -67,7 +60,7 @@ app.delete("/repositories/:id", (request, response) => {
 app.post("/repositories/:id/like", (request, response) => {
   const { id } = request.params;
 
-  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
@@ -75,6 +68,7 @@ app.post("/repositories/:id/like", (request, response) => {
 
   const likes = ++repositories[repositoryIndex].likes;
 
-  return response.json({ likes });
+  return response.json('likes');
 });
+
 module.exports = app;
